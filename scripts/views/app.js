899b333c9756b5fdca5319b182e97910a739585a
Ankai Jie <ankaijie@gmail.com>
2016-08-21 15:31:24 -0400

    revamp whole page start

---
+++ scripts/views/app.js
@@ -0,0 +1,9 @@
+define(['backbone'], function(Backbone) {
+    var App = Backbone.View.extend({
+        initialize: function() {
+            console.log('Started main view');
+        }
+    });
+
+    return App;
+});

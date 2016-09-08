97d531930acef465505878b69bf58b369015e357
Ankai Jie <ankaijie@gmail.com>
2016-09-08 16:46:12 -0400

    begin force simulation

--- src/scripts/views/canvas-d3.js
+++ src/scripts/views/canvas-d3.js
@@ -1,22 +1,36 @@
-define(['backbone', 'underscore', 'jquery', 'd3', 
-	'text!templates/canvas.html'], function(backbone, _, $, d3, canvasTemplate) {
+define(['backbone', 'underscore', 'jquery', 'd3'], function(backbone, _, $, d3) {
 
-	var Canvas = Backbone.View.extend({
+    var Canvas = Backbone.View.extend({
 
-		initialize: function(data) {
-			_.extend(this, data);
-		},
-    	id: 'canvas',
-    	class: 'container main-canvas',
-		template: _.template(canvasTemplate),
+        initialize: function(data) {
+            _.extend(this, data);
+        },
+        id: 'canvas',
+        className: 'container main-canvas',
 
-		render: function() {
-			this.$el.append(this.template(this));
-			//var canvas = d3.select(this.el).append('svg').attr('width', '100%').attr('height', '100%');
-		}
-	});
+        visNodes: [],
+        visEdges: [],
 
-	return Canvas;
+        render: function() {
+            this.canvas = d3.select(this.$el[0]).append('svg').attr('width', '100%').attr('height', '100%');
+            var svg = this.canvas.select('svg');
+            console.log(svg);
+
+            var rootNode = this.nodes.find(function(n) {
+                return n.attributes.root && n.attributes.root === 1;
+            });
+
+            var sim = d3.forceSimulation()
+                .nodes(this.visNodes)
+                .force('charge', d3.forceManyBody())
+                .force('link', d3.forceLink(this.visEdges))
+                .force('center', d3.forceCenter());
+
+            console.log(sim);
+
+        }
+    });
+
+    return Canvas;
 
 });
- 

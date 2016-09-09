124d17eaf2e7a4ef0697960d2a89cdbe43eac3d7
Ankai Jie <ankaijie@gmail.com>
2016-09-09 15:13:18 -0400

    first node finally showing

--- src/scripts/views/index.js
+++ src/scripts/views/index.js
@@ -1,7 +1,7 @@
-define(['backbone', 'views/canvas-d3'], function(backbone, Canvas) {
+define(['backbone', 'views/canvas-d3', 'jquery'], function(backbone, Canvas, $) {
 
     var Index = Backbone.View.extend({
-
+        id: 'mainPage',
         initialize: function(Nodes, Edges) {
             var canvas = new Canvas({ nodes: Nodes, edges: Edges });
             canvas.render();

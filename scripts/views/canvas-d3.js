6c73cd9eddac1b203b4f7356cc71379084ff3c76
Ankai Jie <ankaijie@gmail.com>
2016-09-09 00:39:37 -0400

    initial force sim added

--- scripts/views/canvas-d3.js
+++ scripts/views/canvas-d3.js
@@ -1,36 +1,56 @@
 define(['backbone', 'underscore', 'jquery', 'd3'], function(backbone, _, $, d3) {
 
-    var Canvas = Backbone.View.extend({
+	var Canvas = Backbone.View.extend({
 
-        initialize: function(data) {
-            _.extend(this, data);
-        },
-        id: 'canvas',
-        className: 'container main-canvas',
+		initialize: function(data) {
+			_.extend(this, data);
+		},
+		id: 'canvas',
+		className: 'container main-canvas',
 
-        visNodes: [],
-        visEdges: [],
+		visNodes: [],
+		visEdges: [],
 
-        render: function() {
-            this.canvas = d3.select(this.$el[0]).append('svg').attr('width', '100%').attr('height', '100%');
-            var svg = this.canvas.select('svg');
-            console.log(svg);
+		render: function() {
+			var svg = d3.select(this.$el[0]).append('svg').attr('width', '100%').attr('height', '100%');
+			var $svg = this.$('svg');
+			console.log(svg);
 
-            var rootNode = this.nodes.find(function(n) {
-                return n.attributes.root && n.attributes.root === 1;
-            });
+			var rootNode = this.nodes.find(function(n) {
+				return n.attributes.root && n.attributes.root === 1;
+			});
 
-            var sim = d3.forceSimulation()
-                .nodes(this.visNodes)
-                .force('charge', d3.forceManyBody())
-                .force('link', d3.forceLink(this.visEdges))
-                .force('center', d3.forceCenter());
+			this.visNodes.push(_.extend(rootNode, {fx:0, fy:0}));
 
-            console.log(sim);
+			var simulation = d3.forceSimulation()
+				.force('charge', d3.forceManyBody())
+				.force('link', d3.forceLink(this.visEdges))
+				.force('center', d3.forceCenter());
 
-        }
-    });
+			var edges = svg.append('g')
+				.selectAll('line')
+				.data(this.visEdges)
+				.enter()
+				.append('line')
+				.attr('stroke', 'black');
 
-    return Canvas;
+			var node = svg.append('g')
+				.selectAll('circle')
+				.data(this.visNodes)
+				.enter().append('circle')
+				.attr('r', 30);
+
+			simulation
+				.nodes(this.visNodes);
+
+			simulation.force('link')
+				.links(this.visEdges);
+
+			console.log(simulation);
+
+		}
+	});
+
+	return Canvas;
 
 });

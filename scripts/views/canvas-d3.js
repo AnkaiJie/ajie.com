79ef2912d1b6e634b21ad2d04c92fd2de31c7893
Ankai Jie <ankaijie@gmail.com>
2016-09-11 16:06:03 -0400

    drag workings, but ticking always stops

--- scripts/views/canvas-d3.js
+++ scripts/views/canvas-d3.js
@@ -4,72 +4,90 @@ define(['backbone', 'underscore', 'jquery', 'd3'], function(backbone, _, $, d3) 
 
         initialize: function(data) {
             _.extend(this, data);
-        },
-
-        id: 'canvas',
-
-        visNodes: [],
-        visEdges: [],
-
-        render: function() {
-            var svg = d3.select(this.$el[0]).append('svg').attr('width', '100%').attr('height', '100%'),
-                width = $(window).width(),
-                height = $(window).height();
-
             var rootNode = this.nodes.find(function(n) {
                 return n.attributes.root && n.attributes.root === 1;
             });
+            var width = $(window).width();
+            var height = $(window).height();
 
             this.visNodes.push(_.extend(rootNode, { x: width / 2, y: height * 0.4 }));
+            this.initializeForce();
+        },
 
-            this.simulation = d3.forceSimulation()
-                .force('charge', d3.forceManyBody())
-                .force('link', d3.forceLink(this.visEdges))
-                .force('center', d3.forceCenter());
+        id: 'canvas',
+        visNodes: [],
+        visEdges: [],
 
-            this.edges = svg.append('g')
+        initializeForce: function() {
+            this.simulation = d3.forceSimulation();
+            this.setPhysics();
+            this.render();
+            this.refresh();
+        },
+
+        refresh: function() {
+            if (this.simulation) {
+                this.simulation.stop();
+                this.drawNodes();
+                this.drawEdges();
+                this.simulation
+                    .nodes(this.visNodes)
+                    .force('link', d3.forceLink(this.visEdges));
+                this.simulation.force('link')
+                    .links(this.visEdges);
+                this.simulation.restart();
+            }
+        },
+
+        setPhysics: function() {
+            var dragstarted = function(d) {
+                d3.event.sourceEvent.stopPropagation();
+                d.fx = d.x;
+                d.fy = d.y;
+            };
+
+            function dragged(d) {
+                d.fx = d3.event.x;
+                d.fy = d3.event.y;
+            }
+
+            function dragended(d) {
+                d.fx = null;
+                d.fy = null;
+            }
+
+            this.drag = d3.drag()
+                .on("start", dragstarted)
+                .on("drag", dragged)
+                .on("end", dragended);
+            this.simulation
+                .on('tick', _.bind(this.ticked, this));
+        },
+
+
+        drawNodes: function() {
+
+            this.node = this.svg.append('g')
+                .selectAll('circle')
+                .data(this.visNodes)
+                .enter().append('circle')
+                .attr('r', 30)
+                .call(this.drag);
+
+        },
+
+        drawEdges: function() {
+            this.edge = this.svg.append('g')
                 .selectAll('line')
                 .data(this.visEdges)
                 .enter()
                 .append('line')
                 .attr('stroke', 'black');
-
-            this.nodes = svg.append('g')
-                .selectAll('circle')
-                .data(this.visNodes)
-                .enter().append('circle')
-                .attr('r', 30)
-                .call(d3.drag()
-                    .on('start', this.dragstart)
-                    .on("drag", this.dragged)
-                    .on("end", this.dragended));
-
-            this.simulation
-                .nodes(this.visNodes)
-                .on('tick', this.ticked());
-
-            this.simulation.force('link')
-                .links(this.visEdges);
-
-        },
-
-        dragstarted: function(d) {
-            d.fx = d.x;
-            d.fy = d.y;
-        },
-
-        dragged: function (d) {
-            d.fx = d3.event.x;
-            d.fy = d3.event.y;
-        },
-
-        dragended: function (d) {
-            d.fx = null;
-            d.fy = null;
         },
 
         ticked: function() {
-            this.edges
+            console.log('tick');
+            this.edge
                 .attr("x1", function(d) {
                     return d.source.x;
                 })
@@ -83,14 +101,19 @@ define(['backbone', 'underscore', 'jquery', 'd3'], function(backbone, _, $, d3) 
                     return d.target.y;
                 });
 
-            this.nodes
+            this.node
                 .attr("cx", function(d) {
                     return d.x;
                 })
                 .attr("cy", function(d) {
                     return d.y;
                 });
+        },
+
+        render: function() {
+            this.svg = d3.select(this.$el[0]).append('svg').attr('width', '100%').attr('height', '100%');
         }
+
     });
 
     return Canvas;

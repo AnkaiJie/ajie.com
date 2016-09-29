54fb7022aba9d9c724ca2995089fc67333382a36
Ankai Jie <ankaijie@gmail.com>
2016-09-29 18:07:04 -0400

    added very basic sliding info panel, finished clicking events for sliding panel

--- scripts/views/info-panel.js
+++ scripts/views/info-panel.js
@@ -1,15 +1,45 @@
-define(['backbone', 'underscore', 'jquery'], function(backbone, _, $) {
+define(['backbone', 'underscore', 'jquery',
+    'text!templates/infoPanel.html'
+], function(backbone, _, $, infoTemplate) {
 
     var Panel = Backbone.View.extend({
-    	initialize: function(options) {
-    		_.extend(this, options);
-    		this.listenTo(this.dispatch, 'openInfo', this.handleNodeClick);
-    	},
 
-    	handleNodeClick: function(node){
-    		console.log('In handle click for dispatch');
-    		console.log(node);
-    	}
+        template: _.template(infoTemplate),
+        className: 'info-section',
+
+        initialize: function(options) {
+            _.extend(this, options);
+            this.listenTo(this.dispatch, 'toggleInfo', this.toggleVis);
+            this.listenTo(this.dispatch, 'test', this.test);
+            this.render();
+        },
+
+        toggleVis: function(node) {
+            var $el = this.$el;
+            if (node && node.selected) {
+                this.loadInfo(node);
+                $el.css('width', '600px');
+            } else {
+                $el.css('width', '0px');
+            }
+        },
+
+        loadInfo: function(node) {
+            var $d = $('div .datastuff');
+            $d.empty();
+            var info = node.info;
+            $d.append('<img src="' + info.dpic + '" style="width:304px; height:228px;">')
+            $d.append('<h1>' + info.title + '</h1>')
+            $d.append('<h3>' + info.desc + '</h3>')
+        },
+
+        test: function() {
+            console.log('in test');
+        },
+
+        render: function() {
+            this.$el.html(this.template);
+        }
     });
 
     return Panel;

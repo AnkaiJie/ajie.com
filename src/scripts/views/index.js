6a10a51b59b836bdbc3e62d863f6b578a9575e8d
Ankai Jie <ankaijie@gmail.com>
2016-12-30 01:34:05 -0500

    fix scaling of nodes, grey out expand/collapse if not available

--- src/scripts/views/index.js
+++ src/scripts/views/index.js
@@ -4,9 +4,14 @@ define(['backbone', 'views/canvas-d3', 'jquery', 'views/info-panel', 'views/inst
         var Index = Backbone.View.extend({
             id: 'mainPage',
             initialize: function(Nodes, Edges, Dispatch) {
-                var canvas = new Canvas({ nodes: Nodes, edges: Edges, dispatch: Dispatch });
-                var infoPanel = new InfoPanel({ dispatch: Dispatch });
-                var instruc = new Instruc({ dispatch: Dispatch });
+                this.width = $(window).width();
+                this.height = $(window).height();
+                this.nodeRadius = Math.max(10, Math.min(this.width, this.height) * 0.05);
+
+                var canvas = new Canvas({ nodes: Nodes, edges: Edges, dispatch: Dispatch, 
+                    nodeRadius: this.nodeRadius, width:this.width, height:this.height });
+                var infoPanel = new InfoPanel({ dispatch: Dispatch, nodeRadius: this.nodeRadius });
+                var instruc = new Instruc({ dispatch: Dispatch, nodeRadius: this.nodeRadius });
                 this.$el.append(canvas.$el);
                 this.$el.append(infoPanel.$el);
                 canvas.$el.append(instruc.$el);
